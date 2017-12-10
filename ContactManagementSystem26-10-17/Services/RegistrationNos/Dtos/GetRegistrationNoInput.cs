a84645e85c991a9630a0a24b1291efc66dcd8be1
Raouf <RaoufManzoor@yahoo.com>
2017-12-10 14:39:08 +0500

    Registration No App Service Added And Checked IN

---
+++ ContactManagementSystem26-10-17/Services/RegistrationNos/Dtos/GetRegistrationNoInput.cs
@@ -0,0 +1,8 @@
+﻿namespace ContactManagementSystem26_10_17.Services.RegistrationNos.Dtos
+{
+    public class GetRegistrationNoInput
+    {
+        public int UserId { get; set; }
+        public int Digits { get; set; }
+    }
+}
---
+++ ContactManagementSystem26-10-17/Services/RegistrationNos/RegistrationNoAppService.cs
@@ -0,0 +1,25 @@
+﻿using ContactManagementSystem26_10_17.RegisterOwnerDataBase;
+using ContactManagementSystem26_10_17.Services.RegistrationNos.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManagementSystem26_10_17.Services.RegistrationNos
+{
+    public class RegistrationNoAppService
+    {
+        public dynamic GetAutoRegistrationNo(GetRegistrationNoInput input)
+        {
+            RegisterOwnersContext dbcontext = new RegisterOwnersContext();
+            var currentRegistrationNo = dbcontext.CurrentRegistrationNoes.First();
+            if (input.Digits == 1)
+            {
+                var regNos1 = new RegistrationNoS1();
+
+               var val =   currentRegistrationNo.OneDigit;
+            }
+                return true;
+        }
+    }
+}
