1b5ac8645a3f0538794e324c2ed698228c65f658
Raouf <RaoufManzoor@yahoo.com>
2018-03-10 16:06:14 +0500

    Updated Delete Item History Function

--- ContactManagementSystem26-10-17/Services/Items/ItemAppService.cs
+++ ContactManagementSystem26-10-17/Services/Items/ItemAppService.cs
@@ -433,57 +433,57 @@ namespace ContactManagementSystem26_10_17.Services.Items
                 if (regNoSize == 1)
                 {
                     var regNo = dbcontext.BridgeRegNoItemsS1.Single(e => e.RegId == input.RegNoId);
-                    dbcontext.BridgeRegNoItemsS1.Remove(regNo);
                     dbcontext.Items.Remove(regNo.Item);
+                    dbcontext.BridgeRegNoItemsS1.Remove(regNo);
                     dbcontext.SaveChanges();
                 }
                 else if (regNoSize == 2)
                 {
                     var regNo = dbcontext.BridgeRegNoItemS2.Single(e => e.RegId == input.RegNoId);
-                    dbcontext.BridgeRegNoItemS2.Remove(regNo);
                     dbcontext.Items.Remove(regNo.Item);
+                    dbcontext.BridgeRegNoItemS2.Remove(regNo);
                     dbcontext.SaveChanges();
                 }
                 else if (regNoSize == 3)
                 {
                     var regNo = dbcontext.BridgeRegNoItemsS3.Single(e => e.RegId == input.RegNoId);
-                    dbcontext.BridgeRegNoItemsS3.Remove(regNo);
                     dbcontext.Items.Remove(regNo.Item);
+                    dbcontext.BridgeRegNoItemsS3.Remove(regNo);
                     dbcontext.SaveChanges();
                 }
                 else if (regNoSize == 4)
                 {
                     var regNo = dbcontext.BridgeRegNoItemsS4.Single(e => e.RegId == input.RegNoId);
-                    dbcontext.BridgeRegNoItemsS4.Remove(regNo);
                     dbcontext.Items.Remove(regNo.Item);
+                    dbcontext.BridgeRegNoItemsS4.Remove(regNo);
                     dbcontext.SaveChanges();
                 }
                 else if (regNoSize == 5)
                 {
                     var regNo = dbcontext.BridgeRegNoItemsS5.Single(e => e.RegId == input.RegNoId);
-                    dbcontext.BridgeRegNoItemsS5.Remove(regNo);
                     dbcontext.Items.Remove(regNo.Item);
+                    dbcontext.BridgeRegNoItemsS5.Remove(regNo);
                     dbcontext.SaveChanges();
                 }
                 else if (regNoSize == 6)
                 {
                     var regNo = dbcontext.BridgeRegNoItemsS6.Single(e => e.RegId == input.RegNoId);
-                    dbcontext.BridgeRegNoItemsS6.Remove(regNo);
                     dbcontext.Items.Remove(regNo.Item);
+                    dbcontext.BridgeRegNoItemsS6.Remove(regNo);
                     dbcontext.SaveChanges();
                 }
                 else if (regNoSize == 7)
                 {
                     var regNo = dbcontext.BridgeRegNoItemsS7.Single(e => e.RegId == input.RegNoId);
-                    dbcontext.BridgeRegNoItemsS7.Remove(regNo);
                     dbcontext.Items.Remove(regNo.Item);
+                    dbcontext.BridgeRegNoItemsS7.Remove(regNo);
                     dbcontext.SaveChanges();
                 }
                 else if (regNoSize == 8)
                 {
                     var regNo = dbcontext.BridgeRegNosItemS8.Single(e => e.RegId == input.RegNoId);
-                    dbcontext.BridgeRegNosItemS8.Remove(regNo);
                     dbcontext.Items.Remove(regNo.Item);
+                    dbcontext.BridgeRegNosItemS8.Remove(regNo);
                     dbcontext.SaveChanges();
                 }
 
