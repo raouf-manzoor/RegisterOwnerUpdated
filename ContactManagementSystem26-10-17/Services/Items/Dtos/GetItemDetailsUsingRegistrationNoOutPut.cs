1fcc18dddc3c35eba3bae3a103f8e2c387f2c78b
Raouf <RaoufManzoor@yahoo.com>
2018-03-10 15:34:36 +0500

    item deleted from Item Table in deleteItemHistory

---
+++ ContactManagementSystem26-10-17/Services/Items/Dtos/GetItemDetailsUsingRegistrationNoOutPut.cs
@@ -0,0 +1,16 @@
+﻿using ContactManagementSystem26_10_17.RegisterOwnerDataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManagementSystem26_10_17.Services.Items.Dtos
+{
+    public class GetItemDetailsUsingRegistrationNoOutPut
+    {
+        public Item Item { get; set; }
+        public int? UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
